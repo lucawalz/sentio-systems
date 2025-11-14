a47a6af8fe15af4c8dbb2ee88f519d38516094c8
Lilly Luna Polzer lp120 <lp120.@hdm-stuttgart.de>
2025-11-14 14:27:44 +0100

    #REQ-004: -created component login-box -styled login-box

--- sentio-web/src/components/login/login-box.tsx
+++ sentio-web/src/components/login/login-box.tsx
@@ -1,12 +1,41 @@
+import {Link} from "react-router-dom";
 
 export function LoginBox() {
+
+
     return (
-        <section className="cta-section relative py-40 px-6 overflow-hidden">
+        <div>
+            {/* Background */}
             <div className="absolute inset-0 bg-gradient-to-t from-black via-gray-900 to-black"/>
-            <div className="absolute inset-0">
-                <h2>Login to your account</h2>
+
+            {/* Login-Box */}
+            <div
+                className="relative w-full max-w-md p-10 bg-gray-900/40 border border-gray-700 rounded-xl backdrop-blur text-center">
+                <h2 className="text-3xl font-bold mb-6">Login to your account</h2>
+
+                <input
+                    type="text"
+                    placeholder="e-mail"
+                    className="w-full p-3 mb-4 rounded-lg text-black bg-white"
+                />
+                <input
+                    type="text"
+                    placeholder="passwort"
+                    className="w-full p-3 mb-4 rounded-lg text-black bg-white"
+                />
+
+                <button className="w-full mt-4 px-6 py-3 bg-white text-black rounded-lg font-semibold">
+                    Login
+                </button>
+
+                <Link classname="w-full mt-4 px6 py-3 text-white" to="/createAccount">
+                    <p>Create your own account</p>
+                </Link>
+
             </div>
-        </section>
+        </div>
+
+
 
     )
 }
--- sentio-web/src/pages/LogIn.tsx
+++ sentio-web/src/pages/LogIn.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {LoginBox} from "../components/login/login-box.tsx";
+import {Navigation} from "../components/layout/navigation.tsx"
 
 
 const LogIn = () => {
 
 
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="relative min-h-screen bg-black text-white flex items-center justify-center">
+
             <LoginBox/>
+            <Navigation/>
         </div>
 
     );
