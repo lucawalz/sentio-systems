7ea997eafac7165c459c53b9adbaa74d99b2eaff
Lilly Luna Polzer lp120 <lp120.@hdm-stuttgart.de>
2025-12-02 15:23:49 +0100

    #REQ-023: -add: Contact Page

--- sentio-web/src/pages/Contact.tsx
+++ sentio-web/src/pages/Contact.tsx
@@ -3,10 +3,17 @@ export default function Contact(){
 
     return(
         <>
-        <div className="bg-black text-white">
-            <Navigation/>
+            <div className="absolute inset-0 bg-gradient-to-t from-black via-gray-900 to-black"/>
+            <div className="relative min-h-screen bg-black text-white flex items-center justify-center">
+            <div className="relative w-full max-w-md p-10 bg-gray-900/40 border border-gray-700 rounded-xl backdrop-blur text-center">
+                <div>
+                    <h1 className="text-3xl font-bold mb-6 text-white ">Contact</h1>
+                </div>
 
-        </div>
+
+            </div>
+            </div>
+            <Navigation/>
         </>
     )
 }
