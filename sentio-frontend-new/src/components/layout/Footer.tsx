98bab39c8fb48f173f7aec81d000194227b49392
Lilly Luna Polzer lp120 <lp120.@hdm-stuttgart.de>
2026-01-06 21:48:45 +0100

    #REQ-14: -add: links to footer

--- sentio-frontend-new/src/components/layout/Footer.tsx
+++ sentio-frontend-new/src/components/layout/Footer.tsx
@@ -68,6 +68,7 @@ export default function FooterSection() {
                     <Logo />
                 </Link>
 
+                {/*Links*/}
                 <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
                     {links.map((link, index) => (
                         <Link
@@ -82,7 +83,7 @@ export default function FooterSection() {
                 <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
                     {/*Twitter*/}
                     <a
-                        href="#"
+                        href="https://x.com"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="X/Twitter"
@@ -100,7 +101,7 @@ export default function FooterSection() {
                     </a>
                     {/*LinkedIn*/}
                     <a
-                        href="#"
+                        href="https://de.linkedin.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="LinkedIn"
@@ -118,7 +119,7 @@ export default function FooterSection() {
                     </a>
                     {/*Facebook*/}
                     <a
-                        href="#"
+                        href="https://www.facebook.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="Facebook"
@@ -136,7 +137,7 @@ export default function FooterSection() {
                     </a>
                     {/*Threads*/}
                     <a
-                        href="#"
+                        href="https://www.threads.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="Threads"
@@ -159,7 +160,7 @@ export default function FooterSection() {
                     </a>
                     {/*Instagram*/}
                     <a
-                        href="#"
+                        href="https://www.instagram.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="Instagram"
@@ -175,9 +176,9 @@ export default function FooterSection() {
                                 d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"></path>
                         </svg>
                     </a>
-                    {/*Toktok*/}
+                    {/*Tiktok*/}
                     <a
-                        href="#"
+                        href="https://www.tiktok.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="TikTok"
